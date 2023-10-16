ae683486b94cb53a49fdb94ff5d153f5ccdd46d8
omer <omer.aynaci@hva.nl>
2023-10-16 17:13:17 +0200

    routes defined for customer

--- src/main/kotlin/com/example/models/Customer.kt
+++ src/main/kotlin/com/example/models/Customer.kt
@@ -2,5 +2,12 @@ package com.example.models
 
 import kotlinx.serialization.Serializable
 
+/**
+ * @author Ömer Aynaci
+ * data class for storing a customer
+ * the Serializble annotation generates the JSON representation that we need.
+ */
 @Serializable
 data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
+
+val customerStorage = mutableListOf<Customer>()
